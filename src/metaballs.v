61442562179f752c43cc90abc28b3074af3e6845
Johannes Hoff <johshoff@gmail.com>
2024-09-06 20:27:09 +0200

    Smoother movement with fixpoint coordinates
    Offset balls in x, since their paths otherwise won't intersect much with more
    precision.

--- src/metaballs.v
+++ src/metaballs.v
@@ -85,8 +85,10 @@ module ball
 );
 	reg[7:0] bs[0:255];
 
-	reg[9:0] ball_x = START_X;
-	reg[9:0] ball_y = START_Y;
+	reg[11:0] ball_fix_x = { START_X, 2'b0 };
+	reg[11:0] ball_fix_y = { START_Y, 2'b0 };
+	wire[9:0] ball_x = ball_fix_x[11:2];
+	wire[9:0] ball_y = ball_fix_y[11:2];
 	reg[9:0] ball_vx = 0;
 	reg[9:0] ball_vy = 0;
 
@@ -100,15 +102,15 @@ module ball
 	        : y >= ball_y + 128 ? 0
 	        : bs[{ dy[6] ? ~dy[5:2] : dy[5:2], dx[6] ? ~dx[5:2] : dx[5:2] }];
 
-	wire[9:0] next_x = ball_x + { {2{ball_vx[9]}}, ball_vx[9:2] };
-	wire[9:0] next_y = ball_y + { {2{ball_vy[9]}}, ball_vy[9:2] };
+	wire[11:0] next_x = ball_fix_x + { {2{ball_vx[9]}}, ball_vx };
+	wire[11:0] next_y = ball_fix_y + { {2{ball_vy[9]}}, ball_vy };
 
 	always @(posedge v_sync) begin
-		ball_x <= next_x;
-		ball_y <= next_y;
+		ball_fix_x <= next_x;
+		ball_fix_y <= next_y;
 
-		ball_vx <= ball_vx + (next_x < (SCREEN_WIDTH -128)/2 ? 1 : -1);
-		ball_vy <= ball_vy + (next_y < (SCREEN_HEIGHT-128)/2 ? 1 : -1);
+		ball_vx <= ball_vx + (next_x < {(SCREEN_WIDTH -128)/2, 2'b0 } ? 1 : -1);
+		ball_vy <= ball_vy + (next_y < {(SCREEN_HEIGHT-128)/2, 2'b0 } ? 1 : -1);
 	end
 
 	initial begin
@@ -388,7 +390,7 @@ module metaballs
 	wire[7:0] out_0;
 	ball #(.SCREEN_WIDTH(SCREEN_WIDTH), .SCREEN_HEIGHT(SCREEN_HEIGHT), .START_X((SCREEN_WIDTH-128)*1/3), .START_Y((SCREEN_HEIGHT-128)*1/3)) b_0(x, y, out_0, v_sync);
 	wire[7:0] out_1;
-	ball #(.SCREEN_WIDTH(SCREEN_WIDTH), .SCREEN_HEIGHT(SCREEN_HEIGHT), .START_X((SCREEN_WIDTH-128)*2/3), .START_Y((SCREEN_HEIGHT-128)*12/30)) b_1(x, y, out_1, v_sync);
+	ball #(.SCREEN_WIDTH(SCREEN_WIDTH), .SCREEN_HEIGHT(SCREEN_HEIGHT), .START_X((SCREEN_WIDTH-128)*19/30), .START_Y((SCREEN_HEIGHT-128)*12/30)) b_1(x, y, out_1, v_sync);
 
 	reg pix = 0;
 	always @(posedge x[0]) begin
